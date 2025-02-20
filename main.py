b5863277264f20dccce663cd1e1032a8a0ca21db
Gerardo Solis <gsolis5114@gmail.com>
2025-02-19 23:40:18 -0800

    Fixed asyncio bug

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import asyncio
-from decimal import FloatOperation
-
 import backend.connect as backend
 
 def set_settings():
@@ -8,22 +6,17 @@ def set_settings():
     ipaddress, port = backend.set_env()
     client = backend.set_client(ipaddress, port)
     return client
-    
-def llm_com(client):
+
+async def main():
+    client = set_settings()
+    print("\n[+] Hello operator!")
     while True:
         operator_message = backend.get_message()
-        asyncio.run(backend.chat(client, operator_message))
+        await backend.chat(client, operator_message)
 
         choice = input("\n\n[i] Are there more questions you would like to ask? (y/n): ")
         if choice.lower() == 'n':
-            
             break
-        asyncio.new_event_loop()
-
-def main():
-    client = set_settings()
-    print("\n[+] Hello operator!")
-    llm_com(client)
     
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
---
+++ runllm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ./.venv/bin/activate
+
+./.venv/bin/python3/ ./main.py
