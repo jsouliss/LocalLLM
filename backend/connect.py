4d7e89ff1961b84bcc56542b30a91820ad2ae332
Gerardo Solis <gsolis5114@gmail.com>
2025-02-21 00:09:49 -0800

    added Model class and more model options

--- backend/connect.py
+++ backend/connect.py
@@ -4,42 +4,48 @@ from ollama import AsyncClient
 
 def logo():
     print(r"""
-____________________________            .____    .____       _____   
-\______   \__    ___\_____  \           |    |   |    |     /     \  
- |       _/ |    |   /   |   \   ______ |    |   |    |    /  \ /  \ 
- |    |   \ |    |  /    |    \ /_____/ |    |___|    |___/    Y    \
- |____|_  / |____|  \_______  /         |_______ |_______ \____|__  /
-        \/                  \/                  \/       \/       \/ """)
+    ____________________________            .____    .____       _____   
+    \______   \__    ___\_____  \           |    |   |    |     /     \  
+     |       _/ |    |   /   |   \   ______ |    |   |    |    /  \ /  \ 
+     |    |   \ |    |  /    |    \ /_____/ |    |___|    |___/    Y    \
+     |____|_  / |____|  \_______  /         |_______ |_______ \____|__  /
+            \/                  \/                  \/       \/       \/""")
     print("\n")
 
-def set_env():
-    load_dotenv()
-    ipaddress = os.getenv('IPADDRESS')
-    port = os.getenv('PORT')
-    return ipaddress, port
+class Model:
+    def __init__(self, llm_name):
+        self.llm_name = llm_name
+    
+    @staticmethod
+    def set_env():
+        load_dotenv()
+        port = os.getenv('PORT')
+        ipaddress = os.getenv('IPADDRESS')
+        return ipaddress, port
 
-def set_client(ipaddress, port):
-    client = (AsyncClient(
-            host=f"http://{ipaddress}:{port}"))
-    return client
+    @staticmethod
+    def set_client(ipaddress, port):
+        client = (AsyncClient(
+                host=f"http://{ipaddress}:{port}"))
+        return client
 
-def get_message():
-    operator_message = input(
-        "[i] Please enter you message: ")
-    return operator_message
+    def get_message(self):
+        operator_message = input(
+            "[i] Please enter you message: ")
+        return operator_message
 
-async def chat(client, user_message):
-    message = {
-        'role':'user',
-        'content':user_message,
-    }
-    print('\n')
-    async for part in await client.chat(
-        model='qwen2.5:latest',
-        messages=[message],
-        stream=True):
-            print(
-                part['message']['content'],
-                end='',
-                flush=True
-            )
+    async def chat(self,client, user_message):
+        message = {
+            'role':'user',
+            'content':user_message,
+        }
+        print('\n')
+        async for part in await client.chat(
+            model=f'{self.llm_name}',
+                messages=[message],
+            stream=True):
+                print(
+                    part['message']['content'],
+                    end='',
+                    flush=True
+                )
--- main.py
+++ main.py
@@ -3,20 +3,38 @@ import backend.connect as backend
 
 def set_settings():
     backend.logo()
-    ipaddress, port = backend.set_env()
-    client = backend.set_client(ipaddress, port)
+    ipaddress, port = backend.Model.set_env()
+    client = backend.Model.set_client(ipaddress, port)
     return client
 
-async def main():
-    client = set_settings()
-    print("\n[+] Hello operator!")
-    while True:
-        operator_message = backend.get_message()
-        await backend.chat(client, operator_message)
+def set_Model():
+    print(
+        """
+    Available models: \n
+        [+] deepseek-r1-abliterated:8b
+        [+] deepseek-r1:8b
+        [+] qwen2.5:latest
+        [+] llama2-uncensored:latest
+        [+] dolphin-mistral:latest
+        """)
+    model_name = input("[+] Please enter the model name: ")
+    return model_name
 
-        choice = input("\n\n[i] Are there more questions you would like to ask? (y/n): ")
-        if choice.lower() == 'n':
-            break
-    
+async def main():
+    model_name = set_Model()
+    client = set_settings()
+    try:
+        rtoModel = backend.Model(model_name)
+        print("[+] Connection successful!")
+        print("\n[+] Hello operator!")
+        while True:
+            operator_message = rtoModel.get_message()
+            await rtoModel.chat(client, operator_message)
+            choice = input("\n\n[i] Are there more questions you would like to ask? (y/n): ")
+            if choice.lower() == 'n':
+                break
+    except Exception as e: 
+        print("[!] An error has occurred", e)
+        
 if __name__ == "__main__":
     asyncio.run(main())
